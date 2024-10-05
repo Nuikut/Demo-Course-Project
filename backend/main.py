965f15301a8a0c154435980a3aa083bede6d7549
nuikut <barlikus@gmail.com>
2024-10-06 02:50:27 +0300

    ready for frontend

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,25 @@ from fastapi.templating import Jinja2Templates
 from fastapi import Request
 from fastapi.security import HTTPBearer
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.middleware.cors import CORSMiddleware
 from auth.utils import decode_jwt
 from database import create_user, validate_user, validate_manager
 
 app = FastAPI()
+
+
+origins = {
+    "http://localhost",
+    "http://localhost:3000",
+}
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 templates = Jinja2Templates(directory="resourses")
@@ -61,5 +76,6 @@ async def login_manager(request: Request):
 @app.get("/me")
 async def me(token: str = Depends(get_current_user)):
     pass
+
 
 uvicorn.run(app)
